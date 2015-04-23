2abcc84c116864cb3ce8e3ba681a8b751d3ee025
Evan You <yyx990803@gmail.com>
2015-04-23 16:37:09 -0400

    upgrade semi to 4.0.0

--- node_modules/semi/node_modules/eslint/node_modules/js-yaml/node_modules/argparse/node_modules/lodash/utility/range.js
+++ node_modules/semi/node_modules/eslint/node_modules/js-yaml/node_modules/argparse/node_modules/lodash/utility/range.js
@@ -8,8 +8,9 @@ var nativeMax = Math.max;
 
 /**
  * Creates an array of numbers (positive and/or negative) progressing from
- * `start` up to, but not including, `end`. If `start` is less than `end` a
- * zero-length range is created unless a negative `step` is specified.
+ * `start` up to, but not including, `end`. If `end` is not specified it is
+ * set to `start` with `start` then set to `0`. If `end` is less than `start`
+ * a zero-length range is created unless a negative `step` is specified.
  *
  * @static
  * @memberOf _
