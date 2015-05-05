8cbbedd891520fe7d5458f4d62da249bc30fd78c
Evan You <yyx990803@gmail.com>
2015-05-04 21:10:37 -0400

    fix stream issue on large files

--- semi.js
+++ semi.js
@@ -1,13 +1,25 @@
 var semi = require('semi')
+var leading = checkLeadingOption()
 var cmd = process.argv[2]
+var file = ''
 
 semi.on('error', function (e) {
   process.stderr.write(
     '[semi error] ' +
-    e.message + ' at ' + e.line + ':' + e.column
+    e.message + ' at ' + e.line + ':' + e.column + '\n'
   )
 })
 
 process.stdin.on('data', function (buffer) {
-  process.stdout.write(semi[cmd](buffer.toString()))
+  file += buffer.toString()
 })
+
+process.stdin.on('end', function () {
+  process.stdout.write(semi[cmd](file))
+})
+
+function checkLeadingOption () {
+  var settings = require('fs').readFileSync(__dirname + '/semi.sublime-settings')
+  settings = JSON.parse(settings)
+  return settings.leading
+}
